5accc083dac4def7294b12b71488408b512761c7
tungpham1029 <tungpham1029@gmail.com>
2020-04-27 16:25:55 +0700

    postgresql-heroku

--- crash_course/settings.py
+++ crash_course/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = 'crash_course.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DEMO_TEST',
-        'USER': 'postgres',
-        'PASSWORD': 'StrongPassword',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
